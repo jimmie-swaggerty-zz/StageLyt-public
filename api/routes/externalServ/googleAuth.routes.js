f138845647376f5ecdc5a1c6a50e85d9f5cbc4ed
jimmie-swaggerty <jimmie.swaggerty@gmail.com>
2021-09-09 13:23:43 -0500

    Fix client id

--- api/routes/externalServ/googleAuth.routes.js
+++ api/routes/externalServ/googleAuth.routes.js
@@ -1,7 +1,7 @@
 
 module.exports = (app) => {
     const { OAuth2Client } = require("google-auth-library");
-    const client = new OAuth2Client(process.env.CLIENT_ID);
+    const client = new OAuth2Client(process.env.REACT_APP_CLIENT_ID);
     const sql = require("../../models/db");
 
     app.post("/api/v1/auth/google", async (req, result) => {
