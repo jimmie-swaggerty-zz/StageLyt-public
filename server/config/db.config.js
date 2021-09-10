d168f5777aee748f2963f3a67b315cd6985d38bb
jimmie-swaggerty <jimmie.swaggerty@gmail.com>
2021-09-10 14:54:25 -0500

    updated node

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,17 +1,3 @@
-// module.exports = {
-//     HOST: "localhost",
-//     USER: "root",
-//     PASSWORD: process.env.MY_PASSWORD,
-//     DB: "bookdiva",
-//     dialect: "mysql",
-//     pool: {
-//       max: 5,
-//       min: 0,
-//       acquire: 30000,
-//       idle: 10000
-//     }
-//   };
-
   module.exports = {
     HOST: "stagelyt.cal8jirlbdvh.us-east-2.rds.amazonaws.com",
     USER: "admin",
