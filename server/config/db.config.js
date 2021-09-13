b6333599cbdfb6e0aeac45921242b0b64e9189f7
Ubuntu <ubuntu@ip-172-31-16-110.us-east-2.compute.internal>
2021-09-13 17:38:47 +0000

    updates

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,13 +1,13 @@
-  module.exports = {
-    HOST: "us-cdbr-east-04.cleardb.com",
-    USER: "bb86c15ad1494c",
-    PASSWORD: "6dc5ac8e",
-    DB: "heroku_ad535df570dec71",
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
+module.exports = {
+	  HOST: "stagelyt.cal8jirlbdvh.us-east-2.rds.amazonaws.com",
+	  USER: "admin",
+	  PASSWORD: process.env.MY_PASSWORD,
+	  DB: "stagelyt",
+	  dialect: "mysql",
+	  pool: {
+		      max: 5,
+		      min: 0,
+		      acquire: 30000,
+		      idle: 10000
+		    }
+};
